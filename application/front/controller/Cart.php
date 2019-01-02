cb9a7a33c0aa315fc3f4549cb05e1fc98110ec15
谢森炯 <xiesenjiong@xiesenjngdeiMac.lan>
2019-01-02 22:45:46 +0800

    合并

--- application/front/controller/Cart.php
+++ application/front/controller/Cart.php
@@ -1,6 +1,7 @@
 <?php 
 	namespace app\front\controller;
 	use think\Controller;
+	use think\Session;
 	use app\front\model\Orderitem as OrderitemModel;
 	use app\front\model\Cart as CartModel;
 
@@ -10,6 +11,12 @@
 	class Cart extends Controller {
 		
 		public function Index() {
+			// if (condition) {
+			// 	# code...
+			// } else {
+			// 	# code...
+			// }
+			
 			$customerId = Session::get('customerId');
 			$list = CartModel::all(['customerId' => $customerId, 'orderId' => 0]);
 
