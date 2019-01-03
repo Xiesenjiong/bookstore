e73869c7cbde58051289b346339fe12123ffd6b7
Catona <846406801@qq.com>
2019-01-03 09:49:29 +0800

    购物车页面调整、排行榜修改

--- application/back/controller/Order.php
+++ application/back/controller/Order.php
@@ -1,6 +1,9 @@
 <?php 
 	namespace app\back\controller;
 	use think\Controller;
+	use think\Session;
+	use app\back\model\Order as OrderModel;
+
 
 	/**
 	 * Order控制器
@@ -8,6 +11,8 @@
 	class Order extends Controller {
 		
 		public function index() {
+			$list = OrderModel::paginate(5);
+			$this->assign('list',$list);
 			return $this->fetch();
 		}
 
--- application/front/controller/Cart.php
+++ application/front/controller/Cart.php
@@ -1,6 +1,7 @@
 <?php 
 	namespace app\front\controller;
 	use think\Controller;
+	use think\Session;
 	use app\front\model\Orderitem as OrderitemModel;
 	use app\front\model\Cart as CartModel;
 
