364cf4322d696b43fb70af3e9f37ea30f07885e4
谢森炯 <375789796@qq.com>
2019-01-07 18:03:56 +0800

    Update database.php

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '120.78.4.92',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'bookstore',
     // 用户名
-    'username'        => 'girst',
+    'username'        => 'root',
     // 密码
-    'password'        => '2VacdLophB91wenm',
+    'password'        => 'root',
     // 端口
     'hostport'        => '',
     // 连接dsn
